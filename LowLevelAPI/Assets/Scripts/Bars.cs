4eebdd2b7d33e2cbe7f11381f4b339be9ba47c9e
OmerFarukSariisik <49116399+OmerFarukSariisik@users.noreply.github.com>
2019-10-16 12:29:06 +0300

    LowLevelAPI online oyun projesi eklendi

---
+++ LowLevelAPI/Assets/Scripts/Bars.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bars : MonoBehaviour
+{
+    private Quaternion rotation;
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+    }
+}
